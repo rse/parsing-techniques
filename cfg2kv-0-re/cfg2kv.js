7e28df10603ee54bd4b29569af1a1949a1b2939c
Ralf S. Engelschall <rse@engelschall.com>
2015-05-31 13:18:31 +0200

    add some comments

--- cfg2kv-0-re/cfg2kv.js
+++ cfg2kv-0-re/cfg2kv.js
@@ -3,12 +3,19 @@ export default class CFG2KV {
 
     cfg2kv (cfg) {
         let kv = ""
+
+        /*  get rid of all end-of-line comments  */
         cfg = cfg.replace(/\/\/[^\r\n]*\r?\n/g, "")
+
+        /*  parse a section block  */
         const parseBlock = (ns, cfg) => {
+            /*  parse sections  */
             cfg = cfg.replace(/([a-zA-Z_][a-zA-Z_0-9]*)\s*\{((?:.|\r?\n)*)\}/, (m, key, block) => {
-                block = parseBlock(ns.concat(key), block)
+                block = parseBlock(ns.concat(key), block) /*  RECURSION  */
                 return ""
             })
+
+            /*  parse property assignments  */
             cfg = cfg.replace(/([a-zA-Z_][a-zA-Z_0-9]*)\s*=\s*(?:(\d+)|"((?:\\"|.)*?)")/g, (m, key, val1, val2) => {
                 let val = val1
                 if (val === undefined)
@@ -16,9 +23,13 @@ export default class CFG2KV {
                 kv += `${[ ...ns, key ].join(".")} ${val}\n`
                 return ""
             })
+
             return cfg
         }
+
+        /*  bootstrap parsing  */
         cfg = parseBlock([], cfg)
+
         return kv
     }
 }
